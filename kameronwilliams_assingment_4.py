dc0b19a7a836ced1452ab62e8e083333bc46c236
KameronWilliams-dev <kamamir_06@hotmail.com>
2025-09-26 18:26:50 -0400

    git add kameronwilliams_assignment_4.py git commit -m "Pass Test Case 1: Initial game setup with required variables

---
+++ kameronwilliams_assingment_4.py
@@ -0,0 +1,12 @@
+student_name='Kameron Williams'
+current_gpa=3.2
+study_hours=25
+social_points=45
+stress_level=30
+print(f'Welcome, {student_name}, to College Life Sim')
+print(f'Here are your starting stats:')
+print(f'GPA: {current_gpa}')
+print(f'Study Hours:{study_hours}')
+print(f"Social Points: {social_points}")
+print(f"Stress Level: {stress_level}")
+
